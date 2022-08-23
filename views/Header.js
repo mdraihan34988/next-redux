fe5f8a77f20b3c83ed12678ee67e109a20dcaded
mdraihan34988 <swammia908@gmail.com>
2022-08-23 10:48:32 +0600

    chore : added nav

--- views/Header.js
+++ views/Header.js
@@ -4,8 +4,7 @@ import Nav from './Nav'
 function Header() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-black-900">Header</h1>
-      <Nav/>
+      <Nav transparent/>
     </>
   )
 }
---
+++ views/Restaurants.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Restaurants() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Restaurants
