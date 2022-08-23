2694fb8b783194f7e1d1002d7db4b233f8fd4da2
alihasan13 <ali.hasan@northsouth.edu>
2022-08-23 14:51:23 +0600

    add restaurent page added

---
+++ pages/addRestaurent.js
@@ -0,0 +1,8 @@
+import AddRestaurents from '../views/AddRestaurents'
+
+export default function addRestaurent() {
+  return (
+    <AddRestaurents />
+  )
+}
+
---
+++ views/AddRestaurents.js
@@ -0,0 +1,119 @@
+import React from "react";
+
+function AddRestaurents({ addRestaurents }) {
+  return (
+    <>
+    <br/>
+    <br/>
+    <div class="flex justify-center items-center">
+    <form class="w-full max-w-lg center ">
+
+    <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-title"
+            >
+              Title
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-title"
+              type="text"
+              placeholder="Title here "
+            />
+            
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-date"
+            >
+              Date
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-date"
+              type="date"
+              placeholder="Date here "
+            />
+            
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6 ">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-openning-time"
+            >
+              Openning Time
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-openning-time"
+              type="text"
+              placeholder="Openning time"
+            />
+            
+          </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-closing-time"
+            >
+              Closing Time 
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-closing-time"
+              type="text"
+              placeholder="Closing time "
+            />
+          </div>
+        </div>
+   
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-image"
+            >
+              Image
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-image"
+              type="file"
+              placeholder="Upload image"
+            />
+          </div>
+          
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-location"
+            >
+              Location
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-location"
+              type="text"
+              placeholder="Location here"
+            />
+          </div>
+          
+        </div>
+      </form>
+
+   </div>
+      
+    </>
+  );
+}
+
+export default AddRestaurents;
